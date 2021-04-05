f8a6270cdeaf646b9e752172ef0bbdab4b8a2312
Andrew Johnston <av.johnst@gmail.com>
2021-04-05 10:58:23 -0600

    Final clean up

--- spec/services/avatar_service_spec.rb
+++ spec/services/avatar_service_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe AvatarService, type: :model do
 
       expect(@avatars.class).to eq(Array)
       expect(@avatars.first.class).to eq(Hash)
+      expect(@avatars.first[:_id].class).to eq(String)
+      expect(@avatars.first[:_id]).to eq('5cf5679a915ecad153ab68fd')
       expect(@avatars.first[:name].class).to eq(String)
+      expect(@avatars.first[:name]).to eq('Chan (Fire Nation admiral)')
       expect(@avatars.first[:allies].class).to eq(Array)
+      expect(@avatars.first[:allies]).to eq(['Ozai'])
       expect(@avatars.first[:enemies].class).to eq(Array)
+      expect(@avatars.first[:enemies]).to eq(['Earth Kingdom'])
       expect(@avatars.first[:affiliation].class).to eq(String)
+      expect(@avatars.first[:affiliation]).to eq('Fire Nation Navy')
       expect(@avatars.size).to eq(25)
     end
 
